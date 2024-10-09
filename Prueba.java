2ea6a3d8f1a7c79ebc15aadb3056771296d39d61
Sandra <karwowskasandra@gmail.com>
2024-10-09 14:03:58 +0200

    nuevo commit

--- Prueba.java
+++ Prueba.java
@@ -2,10 +2,10 @@
 
       public static void main(String args[]) {
          
-         System.out.println("Karwowska");
          System.out.println("25");
          System.out.println("1999");
          System.out.println("Sopa");
+         System.out.println("kelly rowland");
        
       }
 	
